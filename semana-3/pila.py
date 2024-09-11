cf18015edb16551fb0dac77c255fd80ad974dc61
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2024-09-11 23:00:21 +0100

    Pilas y parentesis

--- semana-3/pila.py
+++ semana-3/pila.py
@@ -52,7 +52,7 @@ class PilaBasadaEnListas:
         return self.tamanio() == 0
 
 
-if __name__ == "__main__":
+def probar_pilas():
     # pila = PilaBasadaEnListas()
     pila = PilaBasadaEnListasPrime()
     print(f"{pila.elementos=}")
@@ -82,3 +82,90 @@ if __name__ == "__main__":
     print(f"{valor=}")
 
     print(f"{otra_pila.esta_vacia()=}")  # Deberia ser True
+
+
+def invertir_texto(texto):
+    resultado = ""
+
+    pila = PilaBasadaEnListas()
+
+    for letra in texto:
+        pila.apilar(letra)
+
+    print(f"{pila.elementos=}")
+
+    # for _ in range(pila.tamanio()):
+    #     resultado = resultado + pila.desapilar()
+
+    while not pila.esta_vacia():
+        resultado = resultado + pila.desapilar()
+
+    return resultado
+
+
+def probar_invertir():
+    resultado = invertir_texto("AMOR")
+    print(f"{resultado=}")  # Deberia ser ROMA
+
+
+def validar_parentesis(expresion):
+    pila = PilaBasadaEnListas()
+
+    for parentesis in expresion:
+        if parentesis == "(":
+            pila.apilar(parentesis)
+        elif parentesis == ")":
+            if not pila.esta_vacia():
+                pila.desapilar()
+            else:
+                return False
+
+    print(f"{pila.elementos}")
+
+    return pila.esta_vacia()
+
+
+def probar_parentesis(self):
+    resultado = validar_parentesis("(()())")  # Deberia ser True
+    print(f"{resultado=}")
+
+    resultado = validar_parentesis("(()()))")  # Deberia ser False
+    print(f"{resultado=}")
+
+    resultado = validar_parentesis("())(")  # Deberia ser False
+    print(f"{resultado=}")
+
+
+def validar_parentesis_prime(expresion):
+    pila = PilaBasadaEnListas()
+
+    simbolos_de_entrada = ["(", "{", "["]
+    simbolos_de_cierre = [")", "}", "]"]
+
+    for simbolo_actual in expresion:
+        if simbolo_actual in simbolos_de_entrada:
+            # Para simbolos de apertura.
+            pila.apilar(simbolo_actual)
+        elif simbolo_actual in simbolos_de_cierre:
+            if not pila.esta_vacia():
+                simbolo_en_pila = pila.desapilar()
+
+                if simbolos_de_entrada.index(
+                    simbolo_en_pila
+                ) != simbolos_de_cierre.index(simbolo_actual):
+                    return False
+
+            else:
+                return False
+
+    print(f"{pila.elementos}")
+
+    return pila.esta_vacia()
+
+
+if __name__ == "__main__":
+    resultado = validar_parentesis_prime("[(){{}}()]")
+    print(f"{resultado=}")  # Deberia ser True
+
+    resultado = validar_parentesis_prime("[(])")
+    print(f"{resultado=}")  # Deberia ser False
