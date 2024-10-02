094fd86282b08a4d9adcbe7a46fb1ceba95ddd6e
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2024-10-02 22:41:17 +0100

    Insertar al final en lista doble

--- semana-6/listas_dobles.py
+++ semana-6/listas_dobles.py
@@ -37,19 +37,35 @@ class SecuenciaListaDoble:
             elementos.append(nodo_actual.elemento)
             nodo_actual = nodo_actual.siguiente
 
-        return f"{','.join([str(elemento) for elemento in elementos])}"
+        return f"({','.join([str(elemento) for elemento in elementos])})"
+
+    def insertar_final(self, elemento):
+        nuevo_nodo = NodoDoble(
+            elemento=elemento, anterior=self.final_lista, siguiente=None
+        )
+
+        if self.esta_vacia():
+            self.cabeza_lista = nuevo_nodo
+        else:
+            self.final_lista.siguiente = nuevo_nodo
+
+        self.final_lista = nuevo_nodo
+        self.numero_elementos = self.numero_elementos + 1
 
 
 if __name__ == "__main__":
 
     secuencia = SecuenciaListaDoble()
 
-    secuencia.insertar_frente(0)
-    secuencia.insertar_frente(1)
-    secuencia.insertar_frente(2)
-    secuencia.insertar_frente(3)
+    # secuencia.insertar_frente(0)
+    # secuencia.insertar_frente(1)
+    # secuencia.insertar_frente(2)
+    # secuencia.insertar_frente(3)
 
-    print(f"{secuencia.cabeza_lista.elemento=}")  # Deberia ser: 3
-    print(f"{secuencia.final_lista.elemento=}")  # Deberia ser: 0
+    # print(f"{secuencia.cabeza_lista.elemento=}")  # Deberia ser: 3
+    # print(f"{secuencia.final_lista.elemento=}")  # Deberia ser: 0
 
-    print(f"{secuencia.representar()=}")
+    print(f"{secuencia.representar()=}")  # Salida: (3,2,1,0)
+    secuencia.insertar_final(10)
+    secuencia.insertar_final(20)
+    print(f"{secuencia.representar()=}")  # Salida: (3,2,1,0, 10, 20)
